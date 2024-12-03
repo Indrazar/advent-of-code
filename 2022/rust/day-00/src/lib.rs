8136fb8d2d03a8e2e9d511f43acf6c7fb636b32c
indrazar <110272232+Indrazar@users.noreply.github.com>
2024-12-03 15:32:46 -0500

    2022 rust adding benchamrks+2024-like template

--- 2022/rust/day-00/src/lib.rs
+++ 2022/rust/day-00/src/lib.rs
@@ -6,15 +6,13 @@ pub fn process_part2(input: &str) -> String {
     "works".to_string()
 }
 
-
 #[cfg(test)]
 mod tests {
-    use std::fs;
     use super::*;
 
     #[test]
     fn test_input() {
-        let file = fs::read_to_string("./test-input-1.txt").unwrap();
-        assert_eq!(process_part1(file.as_str()), "works");
+        let file = include_str!("../test-input-1.txt");
+        assert_eq!(process_part1(file), "works");
     }
 }
