d0b5bfe3f60809e1b132340356c4a08486af31e8
indrazar <110272232+Indrazar@users.noreply.github.com>
2024-12-10 13:43:17 -0500

    2024 rust day-10 p2

--- 2024/rust/day-10/src/lib.rs
+++ 2024/rust/day-10/src/lib.rs
@@ -117,8 +117,142 @@ pub fn process_part1(input: &str) -> String {
     total.to_string()
 }
 
+fn flood_with_breadcrumbs(
+    input: Coord,
+    prev_coord: Option<Coord>,
+    prev_height: Option<usize>,
+    data: &MapData,
+    flood_map: Option<HashMap<Coord, Vec<Coord>>>,
+) -> HashMap<Coord, Vec<Coord>> {
+    let mut flood_map: HashMap<Coord, Vec<Coord>> = match flood_map {
+        Some(f_m) => f_m,
+        None => HashMap::new(),
+    };
+    // check current square
+    let tile_height = match data.map.get(&input) {
+        Some(tile_height) => {
+            if prev_height.is_none() || (*tile_height == prev_height.unwrap_or(0) + 1) {
+                // valid next step, continue flooding
+                // println!(
+                //     "flood: {:?} to ({},{}) height: {}",
+                //     prev_height, input.x, input.y, tile_height
+                // );
+                match prev_coord {
+                    Some(p_c) => match flood_map.get_mut(&input) {
+                        Some(v) => {
+                            if !v.contains(&p_c) {
+                                v.push(p_c);
+                            }
+                        }
+                        None => {
+                            flood_map.insert(input, vec![p_c]);
+                        }
+                    },
+                    None => {}
+                };
+                *tile_height
+            } else {
+                // invalid next step, do not continue flooding
+                return flood_map;
+            }
+        }
+        None => return flood_map,
+    };
+    // check upwards
+    if input.y >= 1 {
+        let up = Coord {
+            x: input.x,
+            y: input.y - 1,
+        };
+
+        flood_map =
+            flood_with_breadcrumbs(up, Some(input), Some(tile_height), data, Some(flood_map));
+    }
+    // check downwards
+    let down = Coord {
+        x: input.x,
+        y: input.y + 1,
+    };
+    flood_map = flood_with_breadcrumbs(down, Some(input), Some(tile_height), data, Some(flood_map));
+    // check left
+    if input.x >= 1 {
+        let left = Coord {
+            x: input.x - 1,
+            y: input.y,
+        };
+        flood_map =
+            flood_with_breadcrumbs(left, Some(input), Some(tile_height), data, Some(flood_map));
+    }
+    // check right
+    let right = Coord {
+        x: input.x + 1,
+        y: input.y,
+    };
+    flood_map =
+        flood_with_breadcrumbs(right, Some(input), Some(tile_height), data, Some(flood_map));
+    flood_map
+}
+
+fn count_backwards(input: &Coord, data: &MapData, flood_map: &HashMap<Coord, Vec<Coord>>) -> usize {
+    let mut total = 0;
+    let crumb_list = flood_map.get(&input).unwrap_or(&Vec::new()).clone();
+    if crumb_list.is_empty() {
+        match data.map.get(input) {
+            Some(height) => {
+                if *height != 0 {
+                    //println!("invalid route");
+                    return 0;
+                } else {
+                    //println!("in ({},{}) at start", input.x, input.y);
+                    return 1;
+                }
+            }
+            None => {
+                panic!("should be in the map data")
+            }
+        }
+    } else {
+        for crumb in &crumb_list {
+            let routes_to_start = count_backwards(&crumb, &data, flood_map);
+            // println!(
+            //     "in ({},{}) checking ({},{})",
+            //     input.x, input.y, crumb.x, crumb.y
+            // );
+            total += routes_to_start;
+        }
+        //println!("in ({},{}) there are {} paths", input.x, input.y, total,);
+        total
+    }
+}
+
 pub fn process_part2(input: &str) -> String {
-    "works".to_string()
+    let data = parse(input);
+    let starting_points = match data.heights.get(&0) {
+        Some(l) => l.clone(),
+        None => panic!("we expect some starting points"),
+    };
+    let mut total: usize = 0;
+    for start in starting_points {
+        let mut trail_score: usize = 0;
+        let flood_map = flood_with_breadcrumbs(start, None, None, &data, None);
+        //for (key, value) in &flood_map {
+        //    println!("({},{}): {value:?}", key.x, key.y);
+        //}
+        for ending_point in data.heights.get(&9).expect("we expect some ending points") {
+            let ending_val = count_backwards(ending_point, &data, &flood_map);
+            // println!(
+            //     "for start ({},{}); ending ({},{}): value was {ending_val}",
+            //     start.x, start.y, ending_point.x, ending_point.y,
+            // );
+            trail_score += ending_val;
+        }
+        // println!(
+        //     "start point: ({},{}) has a score of {trail_score}",
+        //     start.x, start.y
+        // );
+        total += trail_score;
+    }
+    total.to_string()
 }
 
 #[cfg(test)]
@@ -129,6 +263,6 @@ mod tests {
     fn test_input() {
         let file = include_str!("../test-input-1.txt");
         assert_eq!(process_part1(file), "36");
-        //assert_eq!(process_part2(file), "81");
+        assert_eq!(process_part2(file), "81");
     }
 }
