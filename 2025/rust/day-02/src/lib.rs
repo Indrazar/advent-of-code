61b652f9e0eb7d349613510d7b343169c0071408
indrazar <110272232+Indrazar@users.noreply.github.com>
2025-12-02 11:52:36 -0500

    2025 rust day-02 complete

---
+++ 2025/rust/day-02/src/lib.rs
@@ -0,0 +1,64 @@
+pub fn process_part1(input: &str) -> String {
+    let mut invalid_ids: Vec<u64> = Vec::new();
+    for group in input.split(',') {
+        let mut range = group.split("-");
+        let left: u64 = range.next().unwrap().parse().unwrap();
+        let right: u64 = range.next().unwrap().parse().unwrap();
+        for value in left..=right {
+            let stringed = format!("{value}");
+            if (stringed.len() % 2) == 0 {
+                let (str_left, str_right) = stringed.split_at(stringed.len() / 2);
+                if str_left == str_right {
+                    invalid_ids.push(value);
+                }
+            }
+        }
+    }
+    invalid_ids.iter().sum::<u64>().to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    let mut invalid_ids: Vec<u64> = Vec::new();
+    for group in input.split(',') {
+        let mut range = group.split("-");
+        let left: u64 = range.next().unwrap().parse().unwrap();
+        let right: u64 = range.next().unwrap().parse().unwrap();
+        for value in left..=right {
+            let stringed = format!("{value}");
+            for window_size in 1..=stringed.len() / 2 {
+                if stringed.len() % window_size == 0 {
+                    let str_chunks: Vec<String> = stringed
+                        .chars()
+                        .collect::<Vec<char>>()
+                        .chunks(window_size)
+                        .map(|chunk| chunk.iter().collect::<String>())
+                        .collect();
+                    let mut all_match = true;
+                    for i in 0..str_chunks.len() - 1 {
+                        if str_chunks[i] != str_chunks[i + 1] {
+                            all_match = false;
+                            continue;
+                        }
+                    }
+                    if all_match {
+                        invalid_ids.push(value);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    invalid_ids.iter().sum::<u64>().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let file = include_str!("../test-input-1.txt");
+        assert_eq!(process_part1(file), "1227775554");
+        assert_eq!(process_part2(file), "4174379265");
+    }
+}
